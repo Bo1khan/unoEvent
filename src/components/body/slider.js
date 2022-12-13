88c448f2b70174b9659e2432792aafbf05c6ca9d
Boburkhon <Bo1khan@mail.ru>
2022-12-13 23:18:27 +0500

    all done

--- src/components/body/slider.js
+++ src/components/body/slider.js
@@ -1,28 +1,48 @@
 import 'slick-carousel';
 
-$('.packets').slideUp();
-$('.info').slideUp();
+$(document).ready(function () {
 
-$('.card__btn').on('click', function (e) {
-   $(this).toggleClass('active');
+   $('.packets').slideUp();
+   $('.info').slideUp();
 
-   if ($(this).hasClass('active')) {
-      $(this).html(`Свернуть пакеты <span class="icon-showup"></span>`);
-      $(this).closest('.card__wrapper').next('.packets').slideDown();
-   } else {
-      $(this).html('Посмотреть пакеты <span class="icon-showdown"></span>');
-      $(this).closest('.card__wrapper').next('.packets').slideUp();
-   }
-})
+   $('.card__btn').on('click', function (e) {
+      $(this).toggleClass('active');
 
-$('.packet__info').on('click', function (e) {
-   e.preventDefault();
-   $(this).next('.info').slideToggle();
-})
+      if ($(this).hasClass('active')) {
+         $(this).html(`Свернуть пакеты <span class="icon-showup"></span>`);
+         $(this).closest('.card__wrapper').next('.packets').slideDown();
+      } else {
+         $(this).html('Посмотреть пакеты <span class="icon-showdown"></span>');
+         $(this).closest('.card__wrapper').next('.packets').slideUp();
+      }
+   })
 
-$('.slider').slick({
-   dots: true,
+   $('.packet__info').on('click', function (e) {
+      e.preventDefault();
+      $(this).next('.info').slideToggle();
+   })
+
+   $('.slider').slick({
+      dots: true,
+   });
+
+   $('.slick-prev').html('<span class="icon-prev"></span>');
+   $('.slick-next').html('<span class="icon-next"></span>');
+
+   $('.packets').slick({
+      dots: true,
+      arrows: false,
+      slidesToShow: 3,
+      responsive: [
+         {
+            breakpoint: 1300,
+            settings: {
+               slidesToShow: 1,
+               variableWidth: true,
+               infinite: false
+            }
+         }
+      ]
+   });
+
 });
-
-$('.slick-prev').html('<span class="icon-prev"></span>');
-$('.slick-next').html('<span class="icon-next"></span>');
